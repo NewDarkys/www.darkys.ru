017eb58b8f9d00c6646a741eb69098795696b5ca
Darkys <youtube_darkrage@mail.ru>
2024-08-26 00:57:21 +0300

    treejs

---
+++ app/treejs/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "treejs | TEST",
+};
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  );
+}
