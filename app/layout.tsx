52e7bd3203d052e77ad5a6dc723a4e1163010b16
Darkys <youtube_darkrage@mail.ru>
2024-12-21 04:48:57 +0300

    seo fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,11 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Darkys.ru | 7EYES",
-  description: "Welcome to 7EYES founded by (Darkys | Даркис)",
+  title: "Darkys.ru | 7EYES - Advanced Anomaly Research",
+  keywords:
+    "анализ аномалий, исследование необъяснимого, Darkys, 7EYES, аномалии, исследования, наука, необъяснимые явления, darkys, даркис, rus_darkys, рус даркис",
+  description:
+    "Welcome to 7EYES founded by (Darkys | Даркис). 7EYES - A leading organization in anomaly research. Explore more on Darkys.ru",
 };
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import IntroScene from "@/components/IntroScene/IntroScene";
 import LoginPrompt from "@/components/LoginPrompt/LoginPrompt";
 import AboutMe from "@/components/AboutMe/AboutMe";
 import VFX from "@/components/VFX/VFX";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Head>
+        <link rel="canonical" href="https://www.darkys.ru/" key="canonical" />
+      </Head>
       <VFX server_src="1server.mp3" />
       <IntroScene video_src="IntroVideo.mp4" />
       <LoginPrompt />
